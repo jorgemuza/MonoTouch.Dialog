8d3e3f4695d38bb9a4296db7aac6806a9c8f5b42
Miguel de Icaza <miguel@gnome.org>
2012-02-08 17:31:36 -0500

    Fix the HtmlElement sizing

--- MonoTouch.Dialog/Elements.cs
+++ MonoTouch.Dialog/Elements.cs
@@ -603,10 +603,11 @@ namespace MonoTouch.Dialog
 			var vc = new WebViewController (this) {
 				Autorotate = dvc.Autorotate
 			};
-			web = new UIWebView (UIScreen.MainScreen.ApplicationFrame){
+
+			web = new UIWebView (UIScreen.MainScreen.Bounds) {
 				BackgroundColor = UIColor.White,
 				ScalesPageToFit = true,
-				AutoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight
+				AutoresizingMask = UIViewAutoresizing.All
 			};
 			web.LoadStarted += delegate {
 				NetworkActivity = true;
@@ -627,6 +628,8 @@ namespace MonoTouch.Dialog
 						"An error occurred:".GetText (), args.Error.LocalizedDescription), null);
 			};
 			vc.NavigationItem.Title = Caption;
+			
+			vc.View.AutosizesSubviews = true;
 			vc.View.AddSubview (web);
 			
 			dvc.ActivateController (vc);
