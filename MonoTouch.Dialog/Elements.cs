fcdea12030d6fee4e1b96d572e389a80cbb3d4ec
Rolf Bjarne Kvinge <rolf@xamarin.com>
2012-03-27 16:47:54 +0200

    Assume that when DateTime.Kind is Unspecified, the date is actually Local. Fixes #3983.
    This is required because NSDate doesn't have the concept of an unspecified time zone,
    so at some point we have to decide what an Unspecified kind is. The implicit conversion
    operators between NSDate and DateTime already assumes that Unspecified means Local, so
    just update the UI to show the DateTime values correctly.
    
    Note that ToLocalTiem does not do this: it assumes the input is Utc if it's
    unspecified, which is why we have to do it explicitly before calling ToLocalTime.

--- MonoTouch.Dialog/Elements.cs
+++ MonoTouch.Dialog/Elements.cs
@@ -1689,8 +1689,17 @@ namespace MonoTouch.Dialog
 			}
 		}
 		
+		protected DateTime GetDateWithKind (DateTime dt)
+		{
+			if (dt.Kind == DateTimeKind.Unspecified)
+				return DateTime.SpecifyKind (dt, DateTimeKind.Local);
+
+			return dt;
+		}
+		
 		public virtual string FormatDate (DateTime dt)
 		{
+			dt = GetDateWithKind (dt);
 			return fmt.ToString (dt) + " " + dt.ToLocalTime ().ToShortTimeString ();
 		}
 		
@@ -1778,7 +1787,7 @@ namespace MonoTouch.Dialog
 		
 		public override string FormatDate (DateTime dt)
 		{
-			return fmt.ToString (dt);
+			return fmt.ToString (GetDateWithKind (dt));
 		}
 		
 		public override UIDatePicker CreatePicker ()
@@ -1796,7 +1805,7 @@ namespace MonoTouch.Dialog
 		
 		public override string FormatDate (DateTime dt)
 		{
-			return dt.ToLocalTime ().ToShortTimeString ();
+			return GetDateWithKind (dt).ToLocalTime ().ToShortTimeString ();
 		}
 		
 		public override UIDatePicker CreatePicker ()
